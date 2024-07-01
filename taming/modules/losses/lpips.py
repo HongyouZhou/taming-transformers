09e0c63d5a32f8e0377a913f69468c34b97f8a0c
cederican <cederic.assmann@campus.tu-berlin.de>
2024-07-01 16:50:24 +0200

    add LPIPS 3D with Warvito model to try out

--- taming/modules/losses/lpips.py
+++ taming/modules/losses/lpips.py
@@ -1,11 +1,11 @@
 """Stripped version of https://github.com/richzhang/PerceptualSimilarity/tree/master/models"""
 
+from collections import namedtuple
+
 import torch
 import torch.nn as nn
 from torchvision import models
-from collections import namedtuple
 
-from taming.util import get_ckpt_path
 
 
 class LPIPS(nn.Module):
@@ -13,44 +13,31 @@ class LPIPS(nn.Module):
     def __init__(self, use_dropout=True):
         super().__init__()
         self.scaling_layer = ScalingLayer()
-        self.chns = [64, 128, 256, 512, 512]  # vg16 features
-        self.net = vgg16(pretrained=True, requires_grad=False)
-        self.lin0 = NetLinLayer(self.chns[0], use_dropout=use_dropout)
-        self.lin1 = NetLinLayer(self.chns[1], use_dropout=use_dropout)
-        self.lin2 = NetLinLayer(self.chns[2], use_dropout=use_dropout)
-        self.lin3 = NetLinLayer(self.chns[3], use_dropout=use_dropout)
-        self.lin4 = NetLinLayer(self.chns[4], use_dropout=use_dropout)
-        self.load_from_pretrained()
+        # self.chns = [64, 128, 256, 512, 512]  # vg16 features
+        self.net = torch.hub.load("Warvito/MedicalNet-models", model="medicalnet_resnet10_23datasets", verbose=False,)
         for param in self.parameters():
             param.requires_grad = False
-
-    def load_from_pretrained(self, name="vgg_lpips"):
-        ckpt = get_ckpt_path(name, "taming/modules/autoencoder/lpips")
-        self.load_state_dict(torch.load(ckpt, map_location=torch.device("cpu")), strict=False)
-        print("loaded pretrained LPIPS loss from {}".format(ckpt))
 
     @classmethod
     def from_pretrained(cls, name="vgg_lpips"):
         if name != "vgg_lpips":
             raise NotImplementedError
         model = cls()
-        ckpt = get_ckpt_path(name)
-        model.load_state_dict(torch.load(ckpt, map_location=torch.device("cpu")), strict=False)
+        model.load_state_dict(
+            torch.hub.load("Warvito/MedicalNet-models", 
+                           model="medicalnet_resnet10_23datasets", 
+                           verbose=False,).state_dict(), strict=False)
         return model
 
     def forward(self, input, target):
-        in0_input, in1_input = (self.scaling_layer(input), self.scaling_layer(target))
+        in0_input, in1_input = normalize_tensor(input), normalize_tensor(target) #(self.scaling_layer(input), self.scaling_layer(target))
+        self.net = self.net.to(input.device)
         outs0, outs1 = self.net(in0_input), self.net(in1_input)
-        feats0, feats1, diffs = {}, {}, {}
-        lins = [self.lin0, self.lin1, self.lin2, self.lin3, self.lin4]
-        for kk in range(len(self.chns)):
-            feats0[kk], feats1[kk] = normalize_tensor(outs0[kk]), normalize_tensor(outs1[kk])
-            diffs[kk] = (feats0[kk] - feats1[kk]) ** 2
-
-        res = [spatial_average(lins[kk].model(diffs[kk]), keepdim=True) for kk in range(len(self.chns))]
-        val = res[0]
-        for l in range(1, len(self.chns)):
-            val += res[l]
+        feats0, feats1 = normalize_tensor(outs0), normalize_tensor(outs1)
+        diffs = (feats0 - feats1) ** 2
+        res = spatial_average_3d(diffs, keepdim=True)
+        val = res
+        
         return val
 
 
@@ -63,13 +50,45 @@ class ScalingLayer(nn.Module):
     def forward(self, inp):
         return (inp - self.shift) / self.scale
 
+class BasicBlock(nn.Module):
+    def __init__(self, chn_in, chn_out, stride=1, downsample=None):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv3d(chn_in, chn_out, kernel_size=3, stride=stride, padding=2, dilation=2, bias=False)
+        self.bn1 = nn.BatchNorm3d(chn_out, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv3d(chn_out, chn_out, kernel_size=3, stride=1, padding=2, dilation=2, bias=False)
+        self.bn2 = nn.BatchNorm3d(chn_out, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+        if downsample is True:
+            self.downsample = nn.Sequential(
+                nn.Conv3d(chn_in, chn_out, kernel_size=1, stride=2, bias=False),
+                nn.BatchNorm3d(chn_out, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
+            )
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
 
 class NetLinLayer(nn.Module):
     """ A single linear layer which does a 1x1 conv """
     def __init__(self, chn_in, chn_out=1, use_dropout=False):
         super(NetLinLayer, self).__init__()
         layers = [nn.Dropout(), ] if (use_dropout) else []
-        layers += [nn.Conv2d(chn_in, chn_out, 1, stride=1, padding=0, bias=False), ]
+        layers += [nn.Conv3d(chn_in, chn_out, 1, stride=1, padding=0, bias=False), ]
         self.model = nn.Sequential(*layers)
 
 
@@ -121,3 +140,6 @@ def normalize_tensor(x,eps=1e-10):
 def spatial_average(x, keepdim=True):
     return x.mean([2,3],keepdim=keepdim)
 
+
+def spatial_average_3d(x: torch.Tensor, keepdim: bool = True) -> torch.Tensor:
+    return x.mean([2, 3, 4], keepdim=keepdim)
